76b7724ab485d1bb2fe8dc2167a2e899bd13e68e
Taha Abbas <taha.abbas@ideofuzion.com>
2026-05-20 19:30:16 +0500

    feat: reown configured

---
+++ app/components/connect-button.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { AppKitButton } from "@reown/appkit/react";
+
+export function ConnectButton() {
+  return <AppKitButton />;
+}
---
+++ app/context/appkit.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { createAppKit } from "@reown/appkit/react";
+import { SolanaAdapter } from "@reown/appkit-adapter-solana/react";
+import { solana, solanaDevnet } from "@reown/appkit/networks";
+
+const projectId = process.env.NEXT_PUBLIC_REOWN_PROJECT_ID;
+
+if (!projectId) {
+  throw new Error(
+    "NEXT_PUBLIC_REOWN_PROJECT_ID is not set. Get one from https://dashboard.reown.com",
+  );
+}
+
+const solanaWeb3JsAdapter = new SolanaAdapter();
+
+const metadata = {
+  name: "Donation DApp",
+  description: "Donations Solana dApp",
+  url:
+    typeof window !== "undefined"
+      ? window.location.origin
+      : "http://localhost:3000",
+  icons: ["https://avatars.githubusercontent.com/u/179229932"],
+};
+
+createAppKit({
+  adapters: [solanaWeb3JsAdapter],
+  networks: [solanaDevnet, solana],
+  defaultNetwork: solanaDevnet,
+  metadata,
+  projectId,
+  features: {
+    analytics: true,
+  },
+});
+
+export function AppKitProvider({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
